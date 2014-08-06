3a83d810ecb467d4d76f23fc95672aa98b1c65d9
takuhito-h <bluesbug@gmail.com>
2014-08-07 00:06:22 +0900

    余計なconsole.logを除去

--- test/test-case/test.js
+++ test/test-case/test.js
@@ -1,8 +1,6 @@
 var target = require("../target");
 var chai = require("../../node_modules/chai/chai");
 
-console.log(target.returnPlus(1,2));
-
 describe("target", function () {
     describe("returnPlus", function () {
         it("２つの引数を足した値を返すこと", function () {
